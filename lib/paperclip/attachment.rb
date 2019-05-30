60faaf10f0036a95b7ebee898114e6ca77942412
jaybedubb <jerry.walsh@gmail.com>
2019-05-30 09:05:55 +0000

    don't post_process if we have already post_processed

--- lib/paperclip/attachment.rb
+++ lib/paperclip/attachment.rb
@@ -465,6 +465,7 @@ module Paperclip
 
     def post_process(*style_args) #:nodoc:
       return if @queued_for_write[:original].nil?
+      return unless @post_processing
       instance.run_paperclip_callbacks(:post_process) do
         instance.run_paperclip_callbacks(:"#{name}_post_process") do
           post_process_styles(*style_args)
