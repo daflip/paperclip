c2b914ab6a59d9e40bca22976ba30e4d0afadd6c
jaybedubb <jerry.walsh@gmail.com>
2022-07-28 11:47:24 +0000

    don't regenerate fingerprint afterwards

--- lib/paperclip/attachment.rb
+++ lib/paperclip/attachment.rb
@@ -219,7 +219,8 @@ module Paperclip
 
       # Reset the file size if the original file was reprocessed.
       instance_write(:file_size,   @queued_for_write[:original].size.to_i)
-      instance_write(:fingerprint, generate_fingerprint(@queued_for_write[:original]))
+      # jbw revisit: don't do this
+      #instance_write(:fingerprint, generate_fingerprint(@queued_for_write[:original]))
     ensure
       uploaded_file.close if close_uploaded_file
     end
